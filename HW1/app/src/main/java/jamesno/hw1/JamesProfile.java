a806b220020dd64be6060abcd537bee690f1e308
James No <james.no@gmail.com>
2017-04-08 21:30:04 -0700

    Instantiated starting files for HW1

---
+++ HW1/app/src/main/java/jamesno/hw1/JamesProfile.java
@@ -0,0 +1,13 @@
+package jamesno.hw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JamesProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_james_profile);
+    }
+}
